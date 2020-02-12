950c23519725224c28f3c5609f8bb7bd2068605b
Donovan <donovan.cotter@detroitlabs.com>
2020-02-12 10:06:12 -0500

    added tsconfig and changed index to ts file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to PhotoSearch!</h1>
+    </div>
+  );
+}
+
+export default App;
